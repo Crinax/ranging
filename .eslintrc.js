1c980fc72442480c86d91053ee26a15f4c2309a7
Crinax <agreedeth@gmail.com>
2021-08-06 20:52:58 +0300

    added numFloatRange, NumberRange.float, Range.floatNum

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "ignorePatterns": [".eslintrc.js", "tests/*.test.js"],
+    "rules": {
+    }
+};
