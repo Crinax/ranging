8c492db38d68b947cf0726a78f45f3ee09371eff
Crinax <agreedeth@gmail.com>
2021-09-14 11:35:09 +0300

    Need fix add function

--- tests/numberRange.test.js
+++ tests/numberRange.test.js
@@ -25,6 +25,39 @@ const tests = [
       map: (x) => x ** 2,
     })] should returns [0, 25, 100, 225, 400]
   `,
+  `#5
+    [...new NumberRange({
+      start: 10,
+      end: 20,
+      filter: (x) => (x % 4 === 0)
+    })] should returns [12, 16, 20]
+  `,
+  `#6
+    [...new NumberRange({
+      start: 10,
+      end: 10.5,
+      step: 0.1,
+      float: true,
+    })] should returns [10, 10.1, 10.2, 10.3, 10.4, 10.5]
+  `,
+  `#7
+    new NumberRange({
+      start: 0.1,
+      end: 2,
+      step: 0.1,
+      float: true
+    }).sum should returns 21
+  `,
+  //! Error Test
+  //TODO: fix it
+  `#8
+    new NumberRange({
+      start: 1,
+      end: 2,
+      step: 0.1,
+      float: true
+    }).sum should returns 16.5
+  `
 ]
 
 console.clear();
