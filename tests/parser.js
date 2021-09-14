43ca7a1ac3096970ed65ccc287a17f855d7d7384
Crinax <agreedeth@gmail.com>
2021-09-14 16:40:59 +0300

    Fix english

--- tests/parser.js
+++ tests/parser.js
@@ -1,5 +1,5 @@
 const parser = (text) => {
-  const parts = text.split('should returns');
+  const parts = text.split('should return');
   const fullParts = [...parts[0].match(/#\d+/), parts[0].split(/#\d+/)[1], parts[1]];
   return {
     num: fullParts[0],
