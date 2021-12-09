80d2b8026a102a8162469c1d249387500bfb80f0
Crinax <agreedeth@gmail.com>
2021-12-10 02:24:07 +0300

    some chages

--- test.ts
+++ test.ts
@@ -4,4 +4,10 @@ const numbers = new NumberRange({ start: 10, end: 15 });
 const chars = new CharRange({ start: 'A', end: 'Z' });
 const merged = new MergeRange({ ranges: [numbers, chars] });
 
-console.log(...new ShuffleRange({ range: merged, count: 5 }));
+console.log([...new ShuffleRange({
+	range: merged,
+	count: 5,
+	picking: 3,
+	filter: (_, index) => (index % 2 === 0),
+	map: (x) => String(x),
+})]);
